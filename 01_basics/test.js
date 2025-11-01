819cb84843ead356f914065050f416b91da0ac46
tusharhub5 <rawatushar1205@gmail.com>
2025-11-01 08:03:41 +0000

    data types

---
+++ 01_basics/test.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code a newer version 
+// alert ("hello");  we are using nodejs , not browser
+
+// data types  1) number , bigint , string ="" , boolean => true or false  , null , undefined , symbol, ;[ primitive data type ] can't change valuses 
+//  non primitive data type [ object , function , array]
+
+console.log(typeof " hello");
+console.log(typeof 18);
+console.log(typeof null);
+// console.log(type of true: false);
