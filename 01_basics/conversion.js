2e1e9434debdc93f0b93a88b8f9148c2106708d9
tusharhub5 <rawatushar1205@gmail.com>
2025-11-02 18:56:39 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let score = null
+console.log(typeof score);
+// console.log((typeof score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+// "33" => 33
+// "33abc " =>nan
+
+
+
+
